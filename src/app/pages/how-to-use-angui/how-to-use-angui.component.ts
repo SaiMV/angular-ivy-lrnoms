3359d88d3f2c3df61889949a35c2bc6b7215bf58
SaiMV <saimv09@gmail.com>
2021-09-22 03:16:52 +0530

    1 file modified

--- src/app/pages/how-to-use-angui/how-to-use-angui.component.ts
+++ src/app/pages/how-to-use-angui/how-to-use-angui.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { AlertMessageService } from 'src/app/anggui/ang-gui-services/common-message-service/alert-message.service';
 
 @Component({
   selector: 'how-to-use-angui',
   templateUrl: './how-to-use-angui.component.html',
   styleUrls: ['./how-to-use-angui.component.css'],
 })
-export class HowToUseAngGUI {}
+export class HowToUseAngGUI {
+  constructor(private alertMessageService: AlertMessageService) {
+    this.showErrorMessage();
+  }
+
+  showErrorMessage() {
+    this.alertMessageService.errorCallBackMsg({
+      alertEnabled: true,
+      isRedAlert: true,
+      message: 'Hi, This is a simple alert.',
+    });
+  }
+}
