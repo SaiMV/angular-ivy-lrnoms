20b7436c7f60ab24af64d943e77c981a83cadda2
SaiMV <saimv09@gmail.com>
2021-09-22 03:18:28 +0530

    1 file modified

--- src/app/anggui/ang-gui.module.ts
+++ src/app/anggui/ang-gui.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngGuiAlertComponent } from './alert/ang-gui-alert.component';
 import { CommonModule } from '@angular/common';
+import { AlertMessageService } from './ang-gui-services/common-message-service/alert-message.service';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
   declarations: [AngGuiAlertComponent],
   exports: [AngGuiAlertComponent],
+  providers: [AlertMessageService],
 })
 export class AngGuiModule {}
