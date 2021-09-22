ff61f2b1f3d70da48eb972f0619842a908657c26
SaiMV <saimv09@gmail.com>
2021-09-23 01:17:41 +0530

    2 files modified, 3 added

--- src/app/anggui/ang-gui.module.ts
+++ src/app/anggui/ang-gui.module.ts
@@ -4,11 +4,12 @@ import { AngGuiAlertComponent } from './alert/ang-gui-alert.component';
 import { CommonModule } from '@angular/common';
 import { AlertMessageService } from './ang-gui-services/common-message-service/alert-message.service';
 import { AngGuiIconsComponent } from './icons/ang-gui-icons.component';
+import { AngGuiNavBarComponent } from './nav-bar/ang-gui-nav-bar.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [AngGuiAlertComponent, AngGuiIconsComponent],
-  exports: [AngGuiAlertComponent, AngGuiIconsComponent,],
+  declarations: [AngGuiAlertComponent, AngGuiIconsComponent,AngGuiNavBarComponent],
+  exports: [AngGuiAlertComponent, AngGuiIconsComponent,AngGuiNavBarComponent],
   providers: [AlertMessageService],
 })
 export class AngGuiModule {}
---
+++ src/app/anggui/nav-bar/ang-gui-nav-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ang-gui-nav-bar',
+  templateUrl: './ang-gui-nav-bar.component.html',
+  styleUrls: ['./ang-gui-nav-bar.component.css'],
+})
+export class AngGuiNavBarComponent {}
