19fbeb03ff97bd3583fb7242ebb0dcc37114c963
SaiMV <saimv09@gmail.com>
2021-09-22 20:50:12 +0530

    3 files modified

--- src/app/anggui/ang-gui.module.ts
+++ src/app/anggui/ang-gui.module.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { AngGuiAlertComponent } from './alert/ang-gui-alert.component';
 import { CommonModule } from '@angular/common';
 import { AlertMessageService } from './ang-gui-services/common-message-service/alert-message.service';
+import { AngGuiIconsComponent } from './icons/ang-gui-icons.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [AngGuiAlertComponent],
-  exports: [AngGuiAlertComponent],
+  declarations: [AngGuiAlertComponent, AngGuiIconsComponent],
+  exports: [AngGuiAlertComponent, AngGuiIconsComponent],
   providers: [AlertMessageService],
 })
 export class AngGuiModule {}
---
+++ src/app/anggui/icons/ang-gui-icons.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ang-gui-icon',
+  templateUrl: './ang-gui-icons.component.html',
+  styleUrls: ['./ang-gui-icons.component.css'],
+})
+export class AngGuiIconsComponent {
+  @Input() iconName: boolean = false;
+
+  constructor() {}
+}
