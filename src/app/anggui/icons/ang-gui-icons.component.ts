202de5457bc18d4f3042d7b30cfdcc61c31e1614
SaiMV <saimv09@gmail.com>
2021-09-23 23:07:37 +0530

    5 files modified, 1 deleted

--- src/app/anggui/icons/ang-gui-icons.component.ts
+++ src/app/anggui/icons/ang-gui-icons.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'ang-gui-icon',
   templateUrl: './ang-gui-icons.component.html',
-  styleUrls: ['./ang-gui-icons.component.css'],
 })
 export class AngGuiIconsComponent {
   @Input() iconName: string = '';
