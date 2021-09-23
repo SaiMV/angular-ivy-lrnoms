f2615aae3b35c9c4ca6e5df77cfb1cd1cfa32776
SaiMV <saimv09@gmail.com>
2021-09-24 00:47:32 +0530

    1 file modified, 3 added, 2 deleted

--- src/app/anggui/ang-gui.module.ts
+++ src/app/anggui/ang-gui.module.ts
@@ -6,6 +6,7 @@ import { AlertMessageService } from './ang-gui-services/common-message-service/a
 import { AngGuiIconsComponent } from './icons/ang-gui-icons.component';
 import { AngGuiNavBarComponent } from './nav-bar/ang-gui-nav-bar.component';
 import { AngGuiButtonsComponent } from './buttons/ang-gui-buttons.component';
+import { AngGuiInputBoxComponent } from './input-box/ang-gui-input-box.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
@@ -14,12 +15,14 @@ import { AngGuiButtonsComponent } from './buttons/ang-gui-buttons.component';
     AngGuiIconsComponent,
     AngGuiNavBarComponent,
     AngGuiButtonsComponent,
+    AngGuiInputBoxComponent,
   ],
   exports: [
     AngGuiAlertComponent,
     AngGuiIconsComponent,
     AngGuiNavBarComponent,
     AngGuiButtonsComponent,
+    AngGuiInputBoxComponent,
   ],
   providers: [AlertMessageService],
 })
---
+++ src/app/anggui/input-box/ang-gui-input-box.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ang-gui-input-box',
+  templateUrl: './ang-gui-input-box.component.html',
+  styleUrls: ['./ang-gui-input-box.component.css'],
+})
+export class AngGuiInputBoxComponent {
+  @Input() type: string = 'nomal';
+  constructor() {}
+}
