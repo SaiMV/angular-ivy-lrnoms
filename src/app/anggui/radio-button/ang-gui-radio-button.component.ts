52afaf253e88edaad376c8cd83dc2f3c56cc9bcf
SaiMV <saimv09@gmail.com>
2021-09-25 01:02:59 +0530

    3 files modified, 3 added

---
+++ src/app/anggui/radio-button/ang-gui-radio-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'ang-gui--radio-button',
+  templateUrl: './ang-gui-radio-button.component.html',
+  styleUrls: ['./ang-gui--radio-button.component.css'],
+})
+export class AnguiRadioButtonComponent implements OnInit {
+  @Input() text: String = 'Radio Button';
+  constructor() {}
+
+  ngOnInit() {}
+}
