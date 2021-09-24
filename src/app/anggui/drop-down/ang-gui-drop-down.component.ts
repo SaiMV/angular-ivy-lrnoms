8bc01d0e5aec2cb338924d15285af5345efe1db6
SaiMV <saimv09@gmail.com>
2021-09-24 23:37:20 +0530

    4 files modified

--- src/app/anggui/drop-down/ang-gui-drop-down.component.ts
+++ src/app/anggui/drop-down/ang-gui-drop-down.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'ang-gui-drop-down',
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AngGuiDropDownComponent {
   toggleDropDown: boolean = false;
+
+  //This is based on key value pair. if you are passing an array then it needs other implementations included
+  @Input() dropDownModel: DropDownModelData[] = [
+    { key: 'noData', value: 'Select' },
+  ];
+
+  @Input() selectedValue = 'Select';
+
+  @Output() onSelectionChanged = new EventEmitter<DropDownModelData>();
+
   constructor() {}
+
+  selectedValueChange(data?: any) {
+    this.selectedValue = data.value;
+    this.onSelectionChanged.emit(data);
+    this.toggleDropDown = !this.toggleDropDown;
+  }
+
+  trackByFunction(index, item) {
+    return index;
+  }
+}
+
+export class DropDownModelData {
+  key: String;
+  value: String;
 }
