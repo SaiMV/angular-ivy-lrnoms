e3174a6e1f52252fb6e5888a0f46729f83e82b3b
SaiMV <saimv09@gmail.com>
2021-10-05 22:53:32 +0530

    1 file modified

--- src/app/anggui/tabs/ang-gui-tabs/ang-gui-tabs.component.ts
+++ src/app/anggui/tabs/ang-gui-tabs/ang-gui-tabs.component.ts
@@ -23,22 +23,22 @@ export class AngGuiTabsComponent implements AfterContentInit {
     }
     this.tabs.push(tab);
   }
-  @ContentChildren(TabComponent) tabs: QueryList<AngGuiTabComponent>;
+  @ContentChildren(AngGuiTabComponent) tabsList: QueryList<AngGuiTabComponent>;
 
   // contentChildren are set
   ngAfterContentInit() {
     // get all active tabs
-    let activeTabs = this.tabs.filter((tab) => tab.active);
+    let activeTabs = this.tabsList.filter((tab) => tab.active);
 
     // if there is no active tab set, activate the first
     if (activeTabs.length === 0) {
-      this.selectTab(this.tabs.first);
+      this.selectTab(this.tabsList.first);
     }
   }
 
   selectTab(tab: AngGuiTabComponent) {
     // deactivate all tabs
-    this.tabs.toArray().forEach((tab) => (tab.active = false));
+    this.tabsList.toArray().forEach((tab) => (tab.active = false));
 
     // activate the tab the user has clicked on.
     tab.active = true;
