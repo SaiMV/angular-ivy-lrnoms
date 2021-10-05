687b16377f2cc830465136f3c76e89e956d3c41d
SaiMV <saimv09@gmail.com>
2021-10-05 22:51:24 +0530

    1 file modified

--- src/app/anggui/tabs/ang-gui-tabs/ang-gui-tabs.component.ts
+++ src/app/anggui/tabs/ang-gui-tabs/ang-gui-tabs.component.ts
@@ -1,19 +1,22 @@
-import { Component, Input ,ContentChildren,
+import {
+  Component,
+  Input,
+  ContentChildren,
   QueryList,
-  AfterContentInit} from '@angular/core';
-import { AngGuiTabComponent } from '../ang-gui-tab-item/ang-gui-tab.component';
+  AfterContentInit,
+} from '@angular/core';
+import { AngGuiTabComponent } from '../ang-gui-tab/ang-gui-tab.component';
 
 @Component({
   selector: 'ang-gui-tabs',
   templateUrl: './ang-gui-tabs.component.html',
   styleUrls: ['./ang-gui-tabs.component.css'],
 })
-export class AngGuiTabsComponent implements AfterContentInit{
+export class AngGuiTabsComponent implements AfterContentInit {
   @Input() type: string = 'nomal';
   tabs: AngGuiTabComponent[] = [];
   constructor() {}
 
-  
   addTab(tab: AngGuiTabComponent) {
     if (this.tabs.length === 0) {
       tab.active = true;
@@ -21,24 +24,23 @@ export class AngGuiTabsComponent implements AfterContentInit{
     this.tabs.push(tab);
   }
   @ContentChildren(TabComponent) tabs: QueryList<AngGuiTabComponent>;
-  
+
   // contentChildren are set
   ngAfterContentInit() {
     // get all active tabs
-    let activeTabs = this.tabs.filter((tab)=>tab.active);
-    
+    let activeTabs = this.tabs.filter((tab) => tab.active);
+
     // if there is no active tab set, activate the first
-    if(activeTabs.length === 0) {
+    if (activeTabs.length === 0) {
       this.selectTab(this.tabs.first);
     }
   }
-  
-  selectTab(tab: AngGuiTabComponent){
+
+  selectTab(tab: AngGuiTabComponent) {
     // deactivate all tabs
-    this.tabs.toArray().forEach(tab => tab.active = false);
-    
+    this.tabs.toArray().forEach((tab) => (tab.active = false));
+
     // activate the tab the user has clicked on.
     tab.active = true;
   }
 }
-
